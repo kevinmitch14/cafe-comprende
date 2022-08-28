8d425bbb64e2921977aae8abc658fd5a91d1b507
Kevin Mitchell <kevinmitch14@gmail.com>
2022-08-28 21:11:02 +0100

    React query setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
 import '../styles/globals.css'
 import { SessionProvider } from 'next-auth/react'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </SessionProvider>)
 }
 
