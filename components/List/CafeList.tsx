780976093a48621762d075e9beace1ccf03ea61e
Kevin Mitchell <kevinmitch14@gmail.com>
2022-12-09 16:39:17 -0300

    Loading indicators

--- components/List/CafeList.tsx
+++ components/List/CafeList.tsx
@@ -5,7 +5,12 @@ import { LoadingSpinner } from "../../utils/LoadingSpinner";
 export const CafeList = () => {
   const { isLoading, isError, error, data } = useCafes();
 
-  if (isLoading) return <LoadingSpinner />;
+  if (isLoading)
+    return (
+      <div className="mt-12">
+        <LoadingSpinner />
+      </div>
+    );
   if (isError) {
     return <span>Error: {error.message}</span>;
   }
--- components/Modal/RateExistingCafeModal.tsx
+++ components/Modal/RateExistingCafeModal.tsx
@@ -4,7 +4,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import { CafeDTO } from "../Cafe/Cafe.types";
 import { ModalLayout } from "./ModalLayout";
-import { LoadingSpinner } from "../../utils/LoadingSpinner";
+import { LoadingDots } from "../../utils/LoadingDots";
 
 type RateExistingModalProps = {
   cafe: CafeDTO;
@@ -84,12 +84,16 @@ export const RateExistingCafeModal = ({
           <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
             <button
               type="button"
-              className="inline-flex w-full min-w-[80px] justify-center rounded-md border border-transparent bg-emerald-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+              className="inline-flex w-full min-w-[80px] items-center justify-center rounded-md border border-transparent bg-emerald-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-emerald-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
               onClick={() => {
                 addCafeFromList.mutate({ ...newCafe, rating });
               }}
             >
-              {addCafeFromList.isLoading ? <LoadingSpinner /> : "Submit"}
+              {addCafeFromList.isLoading ? (
+                <LoadingDots variant="light" />
+              ) : (
+                "Submit"
+              )}
             </button>
             <button
               type="button"
