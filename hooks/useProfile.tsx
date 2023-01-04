fd7932471b1aae075544560e98ae4718273559f1
Kevin Mitchell <kevinmitch14@gmail.com>
2023-01-04 18:36:53 -0300

    useProfile init

---
+++ hooks/useProfile.tsx
@@ -0,0 +1,20 @@
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import { CafeProps, Review } from "../components/Cafe/Cafe.types";
+
+export type Profile = {
+    id: number,
+    username: string,
+    bookmarks: CafeProps[]
+    reviews: Review[]
+}
+
+export const useProfile = () => {
+    return useQuery<Profile, Error>(
+        ["profile"],
+        async () => {
+            return axios.get("/api/accounts").then((res) => res.data);
+        },
+        { refetchOnWindowFocus: false }
+    );
+};
