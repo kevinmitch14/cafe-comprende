ea13912c8b4823decfbb23b79535811bc1f68eea
Kevin Mitchell <kevinmitch14@gmail.com>
2023-01-04 18:37:16 -0300

    Add/Remove bookmark API route

---
+++ pages/api/removeBookmark.ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { prisma } from "../../utils/prisma";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { body } = req;
+    console.log(body);
+    const result = await prisma.account.update({
+        where: {
+            id: 1,
+        },
+        data: {
+            bookmarks: {
+                disconnect: {
+                    place_id: body.place_id
+                }
+            }
+        }
+    })
+    res.json(result)
+}
