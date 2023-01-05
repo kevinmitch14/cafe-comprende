3c67498dd98df9edc2eb09e6675ce9fb7ce3b468
Kevin Mitchell <kevinmitch14@gmail.com>
2023-01-05 18:39:34 -0300

    Spinner refactor to accept size props

--- components/shared/LoadingSpinner.tsx
+++ components/shared/LoadingSpinner.tsx
@@ -1,7 +1,11 @@
-export const LoadingSpinner = () => {
+export type SpinnerProps = {
+  size: 'small' | 'base'
+}
+
+export const LoadingSpinner = ({ size }: SpinnerProps) => {
   return (
     <svg
-      className="mx-auto h-6 w-6 animate-spin text-blue-500"
+      className={`mx-auto ${size === 'small' ? 'h-4 w-4' : 'h-6 w-6'} animate-spin text-blue-500`}
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
