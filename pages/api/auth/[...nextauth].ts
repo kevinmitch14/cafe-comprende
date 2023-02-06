345e56d9fe93f6d72c9e844ee01625c3cdccfe4a
Kevin Mitchell <kevinmitch14@gmail.com>
2023-02-06 09:22:23 -0300

    Auth updates for preview environments

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,30 +1,35 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import {prisma} from "../../../utils/prisma";
+import { prisma } from "../../../utils/prisma";
 import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials"
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
+  adapter:
+    process.env.VERCEL_ENV === "preview" ? undefined : PrismaAdapter(prisma),
   providers: [
     process.env.VERCEL_ENV === "preview"
       ? CredentialsProvider({
-        name: 'Credentials',
-        credentials: {
-          username: { label: "Username", type: "text", placeholder: "jsmith" },
-          password: {  label: "Password", type: "password" }
-        },
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        async authorize() {
-          const user = { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-          if (user) {
-            return user
-          } else {
-            return null
-          }
-        }
-      })
+          name: "Credentials",
+          credentials: {
+            username: {
+              label: "Username",
+              type: "text",
+              placeholder: "jsmith",
+            },
+            password: { label: "Password", type: "password" },
+          },
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          async authorize() {
+            return {
+              id: 1943,
+              name: "J Smith",
+              email: "jsmith@example.com",
+              image: "https://i.pravatar.cc/150?u=jsmith@example.com",
+            };
+          },
+        })
       : GoogleProvider({
           clientId: process.env.GOOGLE_ID as string,
           clientSecret: process.env.GOOGLE_SECRET as string,
