0bba255ee23575d575bdf47a641d5785b0a2178f
Kevin Mitchell <kevinmitch14@gmail.com>
2023-02-11 19:29:57 -0300

    Fix typos and add sign out toast

--- components/shared/Toasts.tsx
+++ components/shared/Toasts.tsx
@@ -1,9 +1,10 @@
 import toast from "react-hot-toast";
 
-export const notifyAddCafe = () => toast.success("Review succesfully added.");
+export const notifyAddCafe = () => toast.success("Review successfully added.");
 export const notifyAddBookmark = () =>
-  toast.success("Bookmark succesfully added.");
+  toast.success("Bookmark successfully added.");
 export const notifyRemoveBookmark = () =>
-  toast.success("Bookmark succesfully removed.");
+  toast.success("Bookmark successfully removed.");
 export const notifyError = (message: string) =>
   toast.error(`Operation failed, please try again. ${message}`);
+export const notifySignedOut = () => toast.success("Successfully logged out.");
