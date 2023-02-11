5bb8c2f7036e2d259c62f57afb77074d7df0baee
Kevin Mitchell <kevinmitch14@gmail.com>
2023-02-11 20:34:14 -0300

    Mock data prop name and 0 activity users

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -28,32 +28,40 @@ export default function Profile(
     id: string;
   };
   const { reviews, bookmarks } = props.userAccount;
-
+  const hasReviews = reviews.length > 0;
+  const hasBookmarks = bookmarks.length > 0;
   return (
     <div className="flex">
       <div>
         <h2 className="font-bold">Reviews</h2>
-        {reviews.map((review: Review) => {
-
-          return (
-            <div key={review.id}>
-              <p>Place ID: {review.place_id}</p>
-              <p>Rating: {review.rating}</p>
-            </div>
-          );
-        })}
+        {hasReviews ? (
+          reviews.map((review: Review) => {
+            return (
+              <div key={review.id}>
+                <p>Place ID: {review.place_id}</p>
+                <p>Rating: {review.rating}</p>
+              </div>
+            );
+          })
+        ) : (
+          <p>No reviews yet!</p>
+        )}
       </div>
       <hr />
       <div>
         <h2 className="font-bold">Bookmarks</h2>
-        {bookmarks.map((bookmark: Bookmark) => {
-          return (
-            <div key={bookmark.place_id}>
-              <p>Place ID: {bookmark.place_id}</p>
-              <p>Name: {bookmark.name}</p>
-            </div>
-          );
-        })}
+        {hasBookmarks ? (
+          bookmarks?.map((bookmark: Bookmark) => {
+            return (
+              <div key={bookmark.place_id}>
+                <p>Place ID: {bookmark.place_id}</p>
+                <p>Name: {bookmark.name}</p>
+              </div>
+            );
+          })
+        ) : (
+          <p>No bookmarks yet!</p>
+        )}
       </div>
     </div>
   );
@@ -77,30 +85,32 @@ export const getServerSideProps: GetServerSideProps<any> = async (context) => {
   if (id === "jsmith@example.com") {
     return {
       props: {
-        reviews: [
-          {
-            id: 6,
-            email: "kevinmitch14@gmail.com",
-            place_id: "ChIJ-7S4cNcmGJYR5Unb2U_q6XU",
-            rating: 4,
-          },
-        ],
-        bookmarks: [
-          {
-            place_id: "ChIJ-7S4cNcmGJYR5Unb2U_q6XU",
-            latitude: "-41.31782359999999",
-            longitude: "-72.9820656",
-            name: "Cassis Cafe",
-            updatedAt: "2023-02-04T15:31:09.449Z",
-          },
-          {
-            place_id: "ChIJ98Er0TGRW0gReN0fnKQRzv0",
-            latitude: "53.2906203",
-            longitude: "-8.986531400000001",
-            name: "Grind Briarhill",
-            updatedAt: "2023-02-05T22:50:49.148Z",
-          },
-        ],
+        userAccount: {
+          reviews: [
+            {
+              id: 6,
+              email: "kevinmitch14@gmail.com",
+              place_id: "ChIJ-7S4cNcmGJYR5Unb2U_q6XU",
+              rating: 4,
+            },
+          ],
+          bookmarks: [
+            {
+              place_id: "ChIJ-7S4cNcmGJYR5Unb2U_q6XU",
+              latitude: "-41.31782359999999",
+              longitude: "-72.9820656",
+              name: "Cassis Cafe",
+              updatedAt: "2023-02-04T15:31:09.449Z",
+            },
+            {
+              place_id: "ChIJ98Er0TGRW0gReN0fnKQRzv0",
+              latitude: "53.2906203",
+              longitude: "-8.986531400000001",
+              name: "Grind Briarhill",
+              updatedAt: "2023-02-05T22:50:49.148Z",
+            },
+          ],
+        },
       },
     };
   }
@@ -124,6 +134,6 @@ export const getServerSideProps: GetServerSideProps<any> = async (context) => {
     props: {
       userAccount: account,
       userSession: session,
-    }
+    },
   };
 };
