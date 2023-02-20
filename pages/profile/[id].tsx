c6ff240641d2a81fb7f20d35f49a82d622685a02
Kevin Mitchell <kevinmitch14@gmail.com>
2023-02-20 11:08:37 -0300

    Add profile info dropdown

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -3,6 +3,20 @@ import { CafeDTO, Review } from "../../components/Cafe/Cafe.types";
 import { prisma } from "../../utils/prisma";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]";
+import Image from "next/image";
+import {
+  UserIcon,
+  ArrowRightOnRectangleIcon,
+} from "@heroicons/react/24/outline";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../../components/ui/dropdown-menu";
+import { signOut } from "next-auth/react";
+import Link from "next/link";
+import { notifySignedOut, notifyError } from "../../components/shared/Toasts";
 
 export type Profile = {
   id: string;
@@ -21,38 +35,87 @@ export default function Profile(
   const { reviews, bookmarks } = props.userAccount;
   const hasReviews = reviews.length > 0;
   const hasBookmarks = bookmarks.length > 0;
+  const css = { maxWidth: "100%", height: "auto" };
+
   return (
-    <div className="flex">
-      <div>
-        <h2 className="font-bold">Reviews</h2>
-        {hasReviews ? (
-          reviews.map((review: Review) => {
-            return (
-              <div key={review.id}>
-                <p>Place ID: {review.place_id}</p>
-                <p>Rating: {review.rating}</p>
-              </div>
-            );
-          })
-        ) : (
-          <p>No reviews yet!</p>
+    <div className="p-4">
+      <div className="flex items-center justify-between">
+        <Link href={"/"}>
+          <Image
+            src="https://cdn.shopify.com/s/files/1/0299/2046/0884/files/SyraCoffee_-_CoffeeThunderboltLogoBlac-Espacio_600x_1_1024x1024.png?v=1624869379"
+            alt="logo"
+            style={css}
+            width={38}
+            height={42}
+            priority={true}
+          />
+        </Link>
+        {props.userSession && (
+          <div>
+            <DropdownMenu>
+              <DropdownMenuTrigger className="flex">
+                <Image
+                  className="rounded-[9999px] mx-auto my-auto"
+                  alt={props.userSession.user?.email || ""}
+                  src={props.userSession.user?.image || ""}
+                  width={32}
+                  height={32}
+                />
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <Link href={`/profile/${props.userSession.user?.email}`}>
+                  <DropdownMenuItem>
+                    <UserIcon className="h-4 w-4 mr-2 stroke-2" />
+                    Profile
+                  </DropdownMenuItem>
+                </Link>
+                <DropdownMenuItem
+                  onClick={() =>
+                    signOut({ redirect: false })
+                      .then(() => notifySignedOut())
+                      .catch((error: Error) => notifyError(error.message))
+                  }
+                >
+                  <ArrowRightOnRectangleIcon className="h-4 w-4 mr-2 stroke-2" />
+                  <span>Log Out</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
         )}
       </div>
-      <hr />
-      <div>
-        <h2 className="font-bold">Bookmarks</h2>
-        {hasBookmarks ? (
-          bookmarks?.map((bookmark: Bookmark) => {
-            return (
-              <div key={bookmark.place_id}>
-                <p>Place ID: {bookmark.place_id}</p>
-                <p>Name: {bookmark.name}</p>
-              </div>
-            );
-          })
-        ) : (
-          <p>No bookmarks yet!</p>
-        )}
+      <div className="flex">
+        <div>
+          <h2 className="font-bold">Reviews</h2>
+          {hasReviews ? (
+            reviews.map((review: Review) => {
+              return (
+                <div key={review.id}>
+                  <p>Place ID: {review.place_id}</p>
+                  <p>Rating: {review.rating}</p>
+                </div>
+              );
+            })
+          ) : (
+            <p>No reviews yet!</p>
+          )}
+        </div>
+        <hr />
+        <div>
+          <h2 className="font-bold">Bookmarks</h2>
+          {hasBookmarks ? (
+            bookmarks?.map((bookmark: Bookmark) => {
+              return (
+                <div key={bookmark.place_id}>
+                  <p>Place ID: {bookmark.place_id}</p>
+                  <p>Name: {bookmark.name}</p>
+                </div>
+              );
+            })
+          ) : (
+            <p>No bookmarks yet!</p>
+          )}
+        </div>
       </div>
     </div>
   );
